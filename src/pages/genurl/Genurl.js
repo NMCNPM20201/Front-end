48c4bbfe343193b93925a74c0e2519d0483e6e42
unknown <nguyendacthai2807@gmail.com>
2021-11-11 00:32:29 +0700

    update

--- src/pages/genurl/Genurl.js
+++ src/pages/genurl/Genurl.js
@@ -1,58 +1,65 @@
 import { GiphyFetch } from "@giphy/js-fetch-api";
 import { useEffect, useRef, useState } from "react";
+import {
+    StompSessionProvider,
+    useSubscription,
+} from "react-stomp-hooks";
 
 export default function Genurl() {
-    const gf = new GiphyFetch("vmqVD48zw7QGC3hKatE5bUSA0cZdXhyM");
-    const [gifTexts, setGifTexts] = useState([]);
-    const [waiting, setWaiting] = useState(true);
+    const SubscribingComponent = () => {
+        const gf = new GiphyFetch("vmqVD48zw7QGC3hKatE5bUSA0cZdXhyM");
+        const [gifTexts, setGifTexts] = useState([]);
+        const [waiting, setWaiting] = useState(true);
 
-    const ws = useRef(null);
+        const ws = useRef(null);
 
-    const getGifTexts = async (text) => {
-        const res = await gf.animate(text, { limit: 1 })
-        setGifTexts(values => res.data);
-        setWaiting(values => false);
-    }
+        const getGifTexts = async (text) => {
+            const res = await gf.animate(text, { limit: 1 })
+            setGifTexts(values => res.data);
+            setWaiting(values => false);
+        }
 
-    const Item = (props) => {
+        const Item = (props) => {
+            return (
+                <div className="gif-item">
+                    <img src={props.url} />
+                </div>
+            );
+        }
+
+        const TextList = (props) => {
+            const items = props.gifs.map((itemData) => {
+                return <Item url={itemData.url} />;
+            });
+            return <div className="text-container">{items}</div>;
+        };
+
+        const onMessage = async (msg) => {
+            setWaiting(values => true);
+            await getGifTexts(msg.body);
+        }
+
+        useSubscription("/topic/test", async (message) => await onMessage(message));
+
         return (
-            <div className="gif-item">
-                <img src={props.url} />
-            </div>
+            <>
+                {!waiting && (
+                    <TextList gifs={gifTexts} />
+                )}
+            </>
         );
     }
 
-    const TextList = (props) => {
-        const items = props.gifs.map((itemData) => {
-            return <Item url={itemData.url} />;
-        });
-        return <div className="text-container">{items}</div>;
-    };
-
-    const onMessage = async (event) => {
-        setWaiting(values => true);
-        await getGifTexts(JSON.parse(event.data).accuracy);
-    }
-
-    useEffect(async () => {
-        ws.current = new WebSocket("ws://localhost:3000/ws");
-        ws.current.onmessage = await onMessage;
-        ws.current.onopen = () => ws.current.send("echo");
-        const interval = setInterval(() => {
-            setWaiting(values => true);
-            ws.current.send("echo");
-        }, 20000);
-        return () => {
-            ws.current.close();
-            clearInterval(interval);
-        }
-    }, []);
-
     return (
         <>
-            {!waiting && (
-                <TextList gifs={gifTexts} />
-            )}
+            <StompSessionProvider
+                url={"https://stream.elite12.de/api/sock"}
+                debug={(str) => {
+                    console.log(str);
+                }}
+            >
+                <SubscribingComponent />
+            </StompSessionProvider>
         </>
     );
 }
